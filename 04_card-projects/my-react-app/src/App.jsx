303dde5a8649f0587053b77402e77b27684384a6
Arpan Choudhary <Arpanchoudhary890@gmail.com>
2026-06-25 16:59:33 +0530

    added the card project

---
+++ 04_card-projects/my-react-app/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+// import {Bookmark} from 'lucide-react'
+
+const App = () => {
+  return (
+    <div className="parent">
+      <div className="card">
+        <div className="top">
+          <img src="https://thumbs.dreamstime.com/b/amazon-logo-white-background-montreal-canada-july-printed-paper-98221126.jpg" alt="" />
+          {/* <button>Save <Bookmark /></button> */}
+        </div>
+        <div className="center"></div>
+        <div className="bottom"></div>
+      </div>
+    </div>
+  )
+}
+
+export default App
